61d6b467a44f2e4d483d7367e872427df8d75859
Viviane Dias <vivianedias@protonmail.com>
2020-10-25 21:33:20 -0300

    Initial portfolio setup

--- src/config/index.js
+++ src/config/index.js
@@ -1,18 +1,17 @@
 module.exports = {
-
-    author: "@konstantinmuenster",
-    siteTitle: "Gatsby Starter Portfolio Minimal",
-    siteShortTitle: "Gatsby", // Used as logo text in header, footer, and splash screen
-    siteDescription: "A modern one-page portfolio with a clean yet expressive design.",
+    author: "@vivianedias",
+    siteTitle: "Viviane Dias - Based in São Paulo",
+    siteShortTitle: "vd.", // Used as logo text in header, footer, and splash screen
+    siteDescription: "Desenvolvedora Full Stack e Estudante. Vivendo em São Paulo.",
     siteUrl: "https://gatsby-starter-portfolio-minimal.netlify.app/",
-    siteLanguage: "en_US",
+    siteLanguage: "pt_BR",
     siteIcon: "src/content/favicon.png", // Relative to gatsby-config file
 
     splashScreen: false, // Set this to true if you want to use the splash screen
 
     // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
     // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
-    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40konstantin.muenster",
+    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40viviane_dias",
     shownArticles: 3,
     
     // There are icons available for the following platforms: 
@@ -20,22 +19,17 @@ module.exports = {
     socialMedia: [
         {
             name: "Linkedin",
-            url: "https://www.linkedin.com/in/konstantin-muenster/"
+            url: "https://www.linkedin.com/in/viviane-p-dias/"
         },
         {
             name: "Medium",
-            url: "https://medium.com/@konstantin.muenster"
+            url: "https://medium.com/@viviane_dias"
         },
         {
             name: "Github",
-            url: "https://github.com/konstantinmuenster/gatsby-starter-portfolio-minimal"
-        },
-        {
-            name: "Behance",
-            url: "https://www.behance.net/konstanmnster"
+            url: "https://github.com/vivianedias"
         },
     ],
-  
     navLinks: {
         menu: [
             {
@@ -56,7 +50,6 @@ module.exports = {
             url: "/#contact",
         }
     },
-
     footerLinks: [
         {
             name: "Privacy",
