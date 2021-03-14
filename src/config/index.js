0535ce6172239404437fba60e46e9c48adbead36
Viviane Dias <vivianedias@protonmail.com>
2021-03-14 15:07:36 -0300

    Fixes integration with medium

--- src/config/index.js
+++ src/config/index.js
@@ -11,7 +11,7 @@ module.exports = {
 
     // You can create your own Medium feed with this rss to json converter: https://rss2json.com/
     // To access your Medium RSS feed, just replace this url with your username: https://medium.com/feed/@{yourname}
-    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40viviane_dias",
+    mediumRssFeed: "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40viviane-dias",
     shownArticles: 3,
     
     // There are icons available for the following platforms: 
@@ -23,7 +23,7 @@ module.exports = {
         },
         {
             name: "Medium",
-            url: "https://medium.com/@viviane_dias"
+            url: "https://medium.com/@viviane-dias"
         },
         {
             name: "Github",
